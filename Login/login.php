2a5d867bd05f887a0b40a27d5266f8360ead5584
Joel-103366239 <89374795+Joel-103366239@users.noreply.github.com>
2021-11-18 11:54:27 +0800

    Add files via upload

---
+++ Login/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body class="container">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <h1 class="display-4"><img src="../Logo/logo.png" class="rounded " alt="logo" height= 70rem width=70em>People's Health Pharmacy</h1>
+            </div>
+        </nav>
+          <div class="container">
+          
+          </div>
+          <div class="container position-relative">
+        <div class="row position-absolute top-50 start-45">
+            <div class="col">
+                <div class="card bg-light m-3" style="width: 20rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">Manager</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">PHP Subang Jaya</h6>
+                      <p class="card-text"><a href="loginAdmin.php">Access</a></p>
+                    </div>
+                  </div>
+                  <div class="card bg-light m-3" style="width: 20rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">Employee</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">PHP Subang Jaya</h6>
+                      <p class="card-text"><a href="loginUser.php">Access</a></p>
+                      </div>
+                        </div>
+                  </div>
+            </div>
+        </div>
+    </body>
+</html>

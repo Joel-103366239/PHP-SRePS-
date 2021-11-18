9b21df5782c1e7abe0c619290d3e274485b454be
Joel-103366239 <89374795+Joel-103366239@users.noreply.github.com>
2021-11-18 11:43:22 +0800

    Add files via upload

---
+++ Login/loginAdmin.php
@@ -0,0 +1,36 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body class="container">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <h1 class="display-4"><img src="../Logo/logo.png" class="rounded " alt="logo" height= 70rem width=70em>People's Health Pharmacy</h1>
+            </div>
+        </nav>
+          <div class="container">
+             <!--Intentionally Empty-->
+          </div>
+        <form method="post" action="adminLogic.php" >
+        <div class="container">
+          <h1 class="h1">Login:  <small class="text-muted">Admin</small></h1>
+          <div class="mb-3">
+              <input type="text" class="form-control" name="aID" placeholder="Username" >
+              <span class="text-danger"><?php if(isset($_SESSION['nameErr'])){ echo $_SESSION['nameErr']; unset($_SESSION['nameErr']); } ?></span>
+          </div>
+          <div class="mb-3">
+              <input type="text" class="form-control" name="aPWD" placeholder="Password">
+              <span class="text-danger"><?php if(isset($_SESSION['pwdErr'])){ echo $_SESSION['pwdErr']; unset($_SESSION['pwdErr']); } ?></span>
+          </div>
+          <div class="mb-3">
+              <input type="submit" class="btn btn-primary" name="submit" >
+              <a href="login.php" class="btn btn-danger float-end">Return</a>
+          </div>
+        </div>
+        <div class="container">
+            <span class="text-danger"><?php if(isset($_SESSION['Err'])){ echo $_SESSION['Err']; unset($_SESSION['Err']); } ?></span>
+        </div>
+    </body>
+</html>

89021626a986119c08dc55ccac54ce94ee22c8d2
Joel-103366239 <89374795+Joel-103366239@users.noreply.github.com>
2021-11-18 14:19:03 +0800

    Add files via upload

---
+++ Error/error.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body class="container">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <h1 class="display-4"><img src="../Logo/logo.png" class="rounded " alt="logo" height= 70rem width=70em>People's Health Pharmacy</h1>
+            </div>
+        </nav>
+          <div class="container">
+             <!--Intentionally Empty-->
+          </div>
+        <div class="container">
+            <h1>Access Denied!</h1>
+            <h2>Must be Admin to access this page!</h2>
+            <a href="../Sales/sales.php">Return To Sales</a>
+            <a href="../Inventory/inventory.php">Return To Inventory</a>
+            <a href="../Main/main.php">Return To Main</a>
+        </div>
+    </body>
+</html>
